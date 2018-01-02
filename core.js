643943a8a2d3a9d538e81e50332c0b2685abe776
Misha Marinenko <misha.marinenko.a@gmail.com>
2018-01-02 16:04:03 +0200

    app.css core.js

--- core.js
+++ core.js
@@ -1,7 +1,11 @@
 var app = new Vue({
     el: '.app',
     data: {
-       
+       menu: [
+           {text: ""},
+           {text: ""},
+           {text: ""}
+       ]
     },
     methods: {
 
